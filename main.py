0334e9cddbe90accc70a8204134b9bcc8675e0b9
CristianoVasconcelos2710 <ze.piqueno.2710@gmail.com>
2024-05-29 17:01:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+n1 = float(input("Digite um número:"))
+n2 = float(input("Digite outro número:"))
+Res = n1+n2
+Res2 = n1-n2
+Res3 = n1*n2
+Res4 = n1/n2
+print(f"Os resultados são:\n{Res}\n{Res2}\n{Res3}\n{Res4:,.2f}")
